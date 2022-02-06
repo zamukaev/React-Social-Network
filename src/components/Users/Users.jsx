d13e4610ef74f4ab906e17b43e32af1f6690169a
muslim <abu.safiia2016@gmail.com>
2022-02-06 22:44:15 +0100

    commit20

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,29 +2,36 @@ import React from "react";
 import styles from './Users.module.css';
 import * as axios from 'axios';
 import userImage from '../../image/01.png';
-const Users = (props) => {
-	const users = props.users;
-	if (users.length === 0) {
+class Users extends React.Component {
+
+	componentDidMount() {
 		axios.get('https://social-network.samuraijs.com/api/1.0/users')
 			.then(response => {
-				props.setUsers(response.data.items);
+				this.props.setUsers(response.data.items);
+				console.log(response.data.items)
 			});
-	}
-	return (
-		<div>
-			{users.map((item) => (
-				<div key={item.id}>
-					<img className={styles.usersImage} src={item.photos.small !== null ? item.photos.small : userImage} alt="" />
-					<h3>{item.name}</h3>
-					<p>{'item.location.city'}</p>
-					<p>{'item.location.country'}</p>
-					<button onClick={() => props.toggleFollow(item.id)} >{item.followed ? 'followed' : 'unfollowed'}</button>
-				</div>
-			))
-			}
 
-		</div >
-	);
+	}
+
+	render() {
+		return (
+			<div>
+				{
+					this.props.users.map((item) => (
+						<div key={item.id}>
+							<img className={styles.usersImage} src={item.photos.small !== null ? item.photos.small : userImage} alt="" />
+							<h3>{item.name}</h3>
+							<p>{'item.location.city'}</p>
+							<p>{'item.location.country'}</p>
+							<button onClick={() => this.props.toggleFollow(item.id)} >{item.followed ? 'followed' : 'unfollowed'}</button>
+						</div>
+					))
+				}
+
+			</div >
+		);
+	}
+
 }
 
 export default Users;
